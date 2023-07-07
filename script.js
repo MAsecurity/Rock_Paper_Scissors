18acbf42af6598ddd81d7829348499937bbc1721
MAsecurity <108958617+MAsecurity@users.noreply.github.com>
2023-07-07 20:27:28 +0100

    Complete technical aspects of rock,paper,scissors
    -Added counters which increment each round depending on who wins
    -Useful for declaring winner of the game

--- script.js
+++ script.js
@@ -29,6 +29,7 @@ let getPlayerChoice = () => {
       continue;
     }else if (result === "rock" || result === "paper" || result === "scissors"){
     // Convert input into lowercase 
+    // set validatedInput to true to break out of while loop
       validatedInput = true;
       return result;
     }
@@ -37,11 +38,11 @@ let getPlayerChoice = () => {
 
 
 
-// create a function which checks for has won against who
+// create a function which checks for who has won against who
 
 function checkWinner(Player, Computer){
 
-// return values such as player and computer so they can be processed by playRound
+// return values such as player and computer or draw depending on the outcome so they can be evaluated by the playRound function
   if (Player === Computer) {
     return  "Draw";
     }else if ( Player === "rock" && Computer === "scissors") {
@@ -55,18 +56,22 @@ function checkWinner(Player, Computer){
     }
 
   }
-// create a function which calls the winner 
+// create a function which calls the checkwinner function and processes its return values and also return values to the main function (game)
+// so that the counters can be updated
 
 function playRound(Player, Computer) {
   const result = checkWinner(Player, Computer);
 
   if (result === "Draw"){
     alert("Draw");
+    return "Draw"
   }else if(result === "Player"){
     alert(`You have won ${Player} beats ${Computer}`);
+    return "Player"
 
   }else {
     alert(`You have lost ${Computer} beats ${Player}`);
+    return "Computer"
 
   }
 
@@ -74,22 +79,32 @@ function playRound(Player, Computer) {
 
 
 }
-
-
 
 // Create the main function for the game
-
 function game(){
+  // initalize the counters
+  let scoreOfPlayer = 0;
+  let scoreOfComputer = 0;
+  // create the for loop which will loop for 5 rounds
   for (let i = 0; i < 5; i++) {
-    playRound(getPlayerChoice(), getComputerChoice());
-
-    
-
+    let result = playRound(getPlayerChoice(), getComputerChoice());
+    // process the return values in playRound and increment on the given situation 
+    if (result === "Player") {
+      scoreOfPlayer++;
+    }else if (result === "Computer"){
+      scoreOfComputer++;
+    }
+  }
+  //Outside the for loop declare who has won the game by using if statements and comparing which counter is greater
+  if (scoreOfComputer > scoreOfPlayer){
+    alert("Computer has won the game");
+  }else if (scoreOfPlayer > scoreOfComputer){
+    alert("Player has won the game");
   }
 
 
-}
 
+}
 
 // Call the main function 
 game()
