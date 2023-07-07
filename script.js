830e92a6e520776642d3c713ced1ff062b9bb2a8
MAsecurity <108958617+MAsecurity@users.noreply.github.com>
2023-07-07 15:41:16 +0100

    Update method for computerchoice

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 // First get computer choice using Math.random function.
 let computerChoice = () => {
-  return Math.floor(Math.random() * 3);
-  // returns values 0 ,1 ,2 which can later intepreted as Rock, Paper , Scissors
+  let calcRandom = Math.floor(Math.random() * 3);
+  // returns values 0 ,1 ,2 which can later be intepreted as Rock, Paper , Scissors using switch statements
+  let result;
+  switch(calcRandom) {
+    case 0:
+      return result = "rock";
+      break;
+    
+    case 1:
+      return result = "paper";
+      break;
+
+    case 2:
+      return result = "scissors";
+      break;
+    
+    
+  }
+
 
 };
 // Get input from user but make them not case sensitive by converting inputs all to lowercase
@@ -10,13 +27,19 @@ let playerChoice = () => {
   let result = userSelection.toLowerCase();
   return result;
 };
-// Write the main function for the game and include a for loop which ranges to 5
-// Begin comparing if user choice is rock and computer is 0 "rock"
-// Make sure to intepret the random values as either rock, paper or scissors
-// Example if (computerChoice is equal to 0 ) && (playerChoice is equal to "rock")
-// { 
-//   return "Both choices were rock"
-// }
+
+
+// Write the main function for the game and include a for loop which ranges to 5 to keep the game going for 5 rounds
+
+function main(computer, player) {
+  for (i = 0; i <= 5; i++) {
+
+  }
+}
+
+
+
+// Make sure to implement counters so that you can find out who wins the rounds 
 
 
 
