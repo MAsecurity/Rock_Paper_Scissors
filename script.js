0d340eac9a607169c9af3e4e1eacaf7b853cda0d
MAsecurity <108958617+MAsecurity@users.noreply.github.com>
2023-07-21 06:11:20 +0100

    Update images when picked
    - Each time the user picks either rock, paper and scissors,
      the images representing those choices will change also.

--- script.js
+++ script.js
@@ -25,21 +25,38 @@ function game () {
     button.addEventListener("click", function () {
       //Extract user picks
       let userChoice = button.textContent;
-
       //Extract computer picks
       let computerOptions = ['rock', 'paper', 'scissors'];
-      let computerRandom = Math.floor(Math.random() * 3);
-      // Will return values such as 0,1,2 
+      let computerRandom = Math.floor(Math.random() * 3); // Will return values such as 0,1,2 
       let computerChoice = computerOptions[computerRandom];
-      console.log(`userChoice ${userChoice} computerChoice ${computerChoice}`);
-      
-
-
+      //update images
+      let playerHand = document.querySelector(".player-hand");
+      let computerHand = document.querySelector(".computer-hand");
+      playerHand.src  = `./images/${button.textContent}.png`;
+      computerHand.src = `./images/${computerChoice}.png`;
+      //Call the compare function
 
     })
   })
   }
   function compare(Player, Computer){
+    let winner = document.querySelector(".winner h1");
+    if (Player === Computer) {
+      winner.textContent = "It is a draw"
+
+    }
+    if (Player === 'rock' && Computer === 'scissors') {
+      winner.textContent = "Player wins";
+      
+    }else {
+      winner.textContent = "Computer wins";
+    }
+    if (Player === "scissors"  && Computer === 'Paper') {
+      winner.textContent = "Player wins";
+    }else {
+      winner.textContent = Computer
+
+    }
 
   }
   // Call the inner functions
